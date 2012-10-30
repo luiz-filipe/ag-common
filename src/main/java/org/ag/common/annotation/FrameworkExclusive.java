ca9d55d044d63677d35e5a4c1f3f1a697dbcaa4a
Luiz <luiz@luizabrahao.com>
2012-10-30 20:29:09 +0000

    Cosmetic change in the FrameworkExclusive annotation

--- src/main/java/org/ag/common/annotation/FrameworkExclusive.java
+++ src/main/java/org/ag/common/annotation/FrameworkExclusive.java
@@ -7,8 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Tells users that the annotated method must not be used in their code, that is
- * it is of exclusive use of the framework itself.
+ * Tells users that the annotated method must not be used in their code, that
+ * is, it is of exclusive use of the framework itself.
  * 
  * If you call any method that has this annotation in your code, you are doing
  * something wrong.
